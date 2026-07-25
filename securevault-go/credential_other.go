542a2f3c21a2a1493d9a675627656ff1c82c656d
Andrii Shevchenko <shewchenkoandriy@gmail.com>
2026-07-25 12:19:41 +0300

    chore: remove securevault-go package and its CI jobs (Go support no longer needed)

--- securevault-go/credential_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package securevault
-
-// GetSecretValue is not supported on non-Windows platforms.
-// It always returns ErrUnsupportedPlatform.
-func GetSecretValue(secretID string) (string, error) {
-	return "", ErrUnsupportedPlatform
-}
--- securevault-go/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/andriyshevchenko/SecureVault/securevault-go
-
-go 1.22
-
-require golang.org/x/sys v0.30.0
